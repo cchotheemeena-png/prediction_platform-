eef4daf2f349f0f194fa92c81b3bffb25691996d
cchotheemeena-png <cchotheemeena@gmail.com>
2026-02-05 10:08:15 -0800

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('verify-otp/', views.VerifyOTPView.as_view(), name='verify_otp'),
+    path('login/', views.LoginView.as_view(), name='login'),
+]
